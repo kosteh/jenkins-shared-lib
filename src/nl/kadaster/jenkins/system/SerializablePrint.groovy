0ffb32c07c940b563c3d0e9acde516d5102dbc88
kosteh <kosteh@users.noreply.github.com>
2017-03-01 14:49:53 +0100

    Update SerializablePrint.groovy

--- src/nl/kadaster/jenkins/system/SerializablePrint.groovy
+++ src/nl/kadaster/jenkins/system/SerializablePrint.groovy
@@ -2,9 +2,10 @@ package nl.kadaster.jenkins.system
 
 class SerializablePrint implements Serializable {
     def lastLine
-    SerializablePrint() {this.lastLine = ""}
+    def steps
+    SerializablePrint(steps) {this.lastLine = ""; this.steps = steps }
     def line (String line) {
         lastLine = "${lastLine}\n${line}"
-        echo "${lastLine}"
+        steps.echo "${lastLine}"
     }
 }
