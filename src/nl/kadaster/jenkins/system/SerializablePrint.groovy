e800baf40c160897ae81a6bb0f90421bd609d4c7
kosteh <kosteh@users.noreply.github.com>
2017-03-01 13:48:58 +0100

    Create SerializablePrint.groovy

---
+++ src/nl/kadaster/jenkins/system/SerializablePrint.groovy
@@ -0,0 +1,9 @@
+package nl.kadaster.jenkins.system
+
+class SerializablePrint implements Serializable {
+    def lastLine
+    SerializablePrint(line) {this.lastLine = line}
+    def line (args) {
+        if 
+  }
+}
