41ccf2ac1021c2b01ecab9b980f015ac357f23f1
kosteh <kosteh@users.noreply.github.com>
2017-03-01 14:50:48 +0100

    Update hans.groovy

--- vars/hans.groovy
+++ vars/hans.groovy
@@ -4,6 +4,6 @@ import nl.kadaster.jenkins.system.SerializablePrint
 def call(String name = 'hans') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    def print = new SerializablePrint()
+    def print = new SerializablePrint(this)
     print.line ("${name}")
 }
